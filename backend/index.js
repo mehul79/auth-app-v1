886e49ecf2a5b3d8df06eaf8a5a360c8acbceb42
mehul79 <mehulrgupta@gmail.com>
2025-03-22 10:11:14 +0530

    added cors thingy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser()); // Parse cookies
 
 app.use(
   cors({
-    origin: ["https://fe-auth-app-v1.vercel.app", "http://localhost:3000"],
+    origin: [`${process.env.NEXT_PUBLIC_URL}`, "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
